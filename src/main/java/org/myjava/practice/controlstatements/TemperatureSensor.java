c90821511241c87b6e3e70ba703a9d3155973032
Sobhan369 <sobhanbabukarri@gmail.com>
2025-06-10 12:51:06 +0530

    temperature sensor

--- src/main/java/org/myjava/practice/controlstatements/TemperatureSensor.java
+++ src/main/java/org/myjava/practice/controlstatements/TemperatureSensor.java
@@ -3,28 +3,35 @@ package org.myjava.practice.controlstatements;
 import java.util.Scanner;
 
 public class TemperatureSensor {
-    public void temperature(double t1,double t2, double t3, double t4, double t5){
-        if(t1 >= 28){
-            System.out.println("t1 is high :"+t1);
-        } else if (t2 >= 28) {
-            System.out.println("t2 is high :"+ t2);
-        }
-        else if (t3 >= 28) {
-            System.out.println("t2 is high :"+ t3);
-        }
-        else if (t4 >= 28) {
-            System.out.println("t2 is high :"+ t4);
-        }
-        else if (t5 >= 28) {
-            System.out.println("t2 is high :"+ t5);
-        }
-        else {
-            System.out.println(" All are below 28 degrees");
-        }
+    public void check(float number){
+       if (number < 18 || number > 28 ){
+           System.out.println("out of temp : "+  number);
+       }
+       if (number > 18 && number < 20){
+           System.out.println("coolest");
+       }
+       if (number > 20 && number < 24){
+           System.out.println("cooler");
+       }
+       if (number > 24 && number < 28){
+           System.out.println("cool");
+       }
+
     }
 
     public static void main(String[] args) {
-        TemperatureSensor temperatureSensor = new TemperatureSensor();
-        temperatureSensor.temperature(21,22,12,36,25);
-    }
+       Scanner scanner = new Scanner(System.in);
+       float f1 = scanner.nextFloat();
+       float f2 = scanner.nextFloat();
+       float f3 = scanner.nextFloat();
+       float f4 = scanner.nextFloat();
+       float f5 = scanner.nextFloat();
+       TemperatureSensor temperatureSensor = new TemperatureSensor();
+       temperatureSensor.check(f1);
+       temperatureSensor.check(f2);
+       temperatureSensor.check(f3);
+       temperatureSensor.check(f4);
+       temperatureSensor.check(f5);
+   }
+
 }
