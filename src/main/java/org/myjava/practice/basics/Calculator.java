af5fbac6d60d97125e028744d3e1ce9f202647b8
Sobhan369 <sobhanbabukarri@gmail.com>
2025-06-10 11:24:23 +0530

    add floor

--- src/main/java/org/myjava/practice/basics/Calculator.java
+++ src/main/java/org/myjava/practice/basics/Calculator.java
@@ -6,6 +6,15 @@ public class Calculator {
     public void add(int a, int b){
         System.out.println(a+b);
     }
+    public void mul(int a, int b){
+        System.out.println(a*b);
+    }
+    public void sub(int a, int b){
+        System.out.println(a-b);
+    }
+    public void div(int a,int b){
+        System.out.println(a/b);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,5 +22,8 @@ public class Calculator {
         int b = scanner.nextByte();
         Calculator calculator = new Calculator();
         calculator.add(a,b);
+     //   calculator.sub(a,b);
+    //    calculator.mul(a,b);
+    //    calculator.div(a,b);
     }
 }
